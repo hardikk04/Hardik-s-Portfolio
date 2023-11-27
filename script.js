9510262d4a39f682397f4dc90ab19d13ae3701e5
hardikk04 <hardik04062003@gmail.com>
2023-11-27 22:21:27 +0530

    all done

--- script.js
+++ script.js
@@ -242,45 +242,6 @@ function sheryJs() {
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
     duration: 1,
   });
-
-  Shery.imageEffect(
-    ".page3-left-part1>img,.page3-left-part2-top>img,.page3-left-part2-bottom>img",
-    {
-      style: 5,
-      // debug: true,
-      config: {
-        a: { value: 2, range: [0, 30] },
-        b: { value: 0.75, range: [-1, 1] },
-        zindex: { value: "9", range: [-9999999, 9999999] },
-        aspect: { value: 0.7266655535147771 },
-        ignoreShapeAspect: { value: true },
-        shapePosition: { value: { x: 0, y: 0 } },
-        shapeScale: { value: { x: 0.5, y: 0.5 } },
-        shapeEdgeSoftness: { value: 0, range: [0, 0.5] },
-        shapeRadius: { value: 0.05, range: [0, 2] },
-        currentScroll: { value: 0 },
-        scrollLerp: { value: 0.07 },
-        gooey: { value: false },
-        infiniteGooey: { value: false },
-        growSize: { value: 4, range: [1, 15] },
-        durationOut: { value: 1, range: [0.1, 5] },
-        durationIn: { value: 1.5, range: [0.1, 5] },
-        displaceAmount: { value: 0.5 },
-        masker: { value: false },
-        maskVal: { value: 1.15, range: [1, 5] },
-        scrollType: { value: 0 },
-        geoVertex: { range: [1, 64], value: 1 },
-        noEffectGooey: { value: true },
-        onMouse: { value: 1 },
-        noise_speed: { value: 0.2, range: [0, 10] },
-        metaball: { value: 0.2, range: [0, 2] },
-        discard_threshold: { value: 0.5, range: [0, 1] },
-        antialias_threshold: { value: 0.002, range: [0, 0.1] },
-        noise_height: { value: 0.5, range: [0, 2] },
-        noise_scale: { value: 10, range: [0, 100] },
-      },
-    }
-  );
 }
 
 sheryJs();
